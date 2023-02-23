534a93467cc578f19ad9e5ada65e1d6a9e79ccc8
Donmark2k <chukwuemekavp@gmail.com>
2023-02-23 05:08:09 +0100

    Add Catergories file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar';
+import Categories from './components/categories'
 
 function App() {
   return (
     <div className="App">
       <>
         <NavBar />
-        <Routes>
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/quotes" element={<Quotes />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        {/* <Routes>
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/books" element={<Book />} />
+        </Routes> */}
       </>
     </div>
   );
---
+++ src/components/categories.js
@@ -0,0 +1,7 @@
+const Categories = () => {
+    return (
+        <button type="button">CHECK STATUS </button>
+    )
+}
+
+export default Categories;
