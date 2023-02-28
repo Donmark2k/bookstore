2eed88dc26f03dd27230cb4ea7ba534cd56b71f0
Donmark2k <chukwuemekavp@gmail.com>
2023-02-28 18:24:43 +0100

    Display the book

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AddBook from './addBook';
 import Book from './Book';
 
-const BookList = () => (
-  <>
-    <Book />
-    <AddBook />
-  </>
-);
+// const BookList = () => {
+//   const { books } = useSelector((state) => state);
+//    console.log(books);
+//   return (
+//     <>
+//       { books.map((book) => <Book key={book.item_id} title={book.title} author={book.author} />) }
+      
+
+//     </>
+//   );
+// };
+
+// export default BookList;
+const BookList = () => {
+  const { books } = useSelector((state) => state);
+
+  return (
+    <> 
+    <div>
+      { 
+        books.books.map((book) => 
+      <Book key={book.item_id} title={book.title} author={book.author}
+       />) 
+       }
+       </div>
+      <AddBook />
+
+    </>
+  );
+};
 
 export default BookList;
+
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,25 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [{
-  item_id: 'item1',
-  title: 'The Great Gatsby',
-  author: 'John Smith',
-  category: 'Fiction',
-},
-{
-  item_id: 'item2',
-  title: 'Anna Karenina',
-  author: 'Leo Tolstoy',
-  category: 'Fiction',
-},
-{
-  item_id: 'item3',
-  title: 'The Selfish Gene',
-  author: 'Richard Dawkins',
-  category: 'Nonfiction',
-},
-];
+const initialState = {
+  numberOfBooks: '4',
+  books: [{
 
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+  ],
+};
 
 const bookSlice = createSlice({
   name: 'books',
