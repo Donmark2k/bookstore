e57ce918900db1a709a5f2d80b25ff8b690036c9
Donmark2k <chukwuemekavp@gmail.com>
2023-03-01 06:29:25 +0100

    Add the propTypes

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
--- src/components/categories.js
+++ src/components/categories.js
@@ -1,18 +1,17 @@
-
 import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categoriesSlice';
 
 const Categories = () => {
   const status = useSelector((state) => state.categories);
   const dispatch = useDispatch();
-  const handleStatus = (id) => {
+  const handleStatus = () => {
     dispatch(checkStatus());
   };
-return (
-  <>
-  <button type="button" onClick={() => handleStatus()} className="statusBtn">CHECK STATUS </button>
-  <span>{status}</span>
-  </>
-);
-}
+  return (
+    <>
+      <button type="button" onClick={() => handleStatus()} className="statusBtn">CHECK STATUS </button>
+      <span>{status}</span>
+    </>
+  );
+};
 export default Categories;
