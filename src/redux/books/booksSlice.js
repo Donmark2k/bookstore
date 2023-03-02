f511705d482669af5b226091b7ff31573fb0cf09
Donmark2k <chukwuemekavp@gmail.com>
2023-03-02 03:36:30 +0100

    Add the API

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,24 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/pjWgrz0ikxleypZxZkv7/books'
 const initialState = {
   books: [],
 };
 
+export const fetchBook = createAsyncThunk('books/fetchBook', async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  const books = [
+    Object.keys(data).map((key) => ({
+      id: key,
+      ...data[key][0],
+    })),
+  ];
+  console.log(books)
+  return books;
+});
+
 const bookSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    addBook: (state, action) => {
-      state.books.push(action.payload);
-    },
-    removeBook: (state, action) => {
-      const { id } = action.payload;
-      const newState = { ...state };
-      newState.books = state.books.filter((book) => book.item_id !== id);
-      return newState;
-    },
-  },
+  reducers: { },
 });
 
 export const { addBook, removeBook } = bookSlice.actions;
